4f4d32b62f280939ce8400661c9715677697f4f6
Somyansh Rana <somyanshrana80@gmail.com>
2026-09-13 01:19:31 +0530

    Add 2 Binary Numbers

--- UniqueThreeDigitEvenNumbers .java
+++ UniqueThreeDigitEvenNumbers .java
@@ -1,7 +1,7 @@
 import java.util.HashSet;
 import java.util.Set;
 
-public class UniqueThreeDigitEvenNumbers {
+class UniqueThreeDigitEvenNumbers {
 
     public static int totalNumbers(int[] digits) {
 
