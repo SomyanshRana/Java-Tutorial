fe75101fe7bbe102012a655f5a9f55e4fa2061f1
Somyansh Rana <somyanshrana80@gmail.com>
2026-09-21 13:15:55 +0530

    Solving diameter, Subtree and Topview Problems of Binary Tree

--- Binary Tree/HeightOfTree.java
+++ Binary Tree/HeightOfTree.java
@@ -43,6 +43,22 @@ public class HeightOfTree {
         return ls + rs + root.data;
     }
 
+    //Diameter of a tree
+    public static int diameter(Node root) {
+        if(root == null) {
+            return 0;
+        }
+
+        int leftDiam = diameter(root.left);
+        int leftHt = height(root.left);
+        int rightDiam = diameter(root.right);
+        int rightHt = height(root.right);
+
+        int selfDiam = leftHt + rightHt + 1;
+
+        return Math.max(selfDiam, (Math.max(leftDiam, rightDiam)));
+    }
+
     public static void main(String args[]) {
         Node root = new Node(1);
         root.left = new Node(2);
@@ -55,5 +71,6 @@ public class HeightOfTree {
         System.out.println(height(root));
         System.out.println(count(root));
         System.out.println(sum(root));
+        System.out.println(diameter(root));
     }
 }
