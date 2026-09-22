eac31e48dd923c91c8048eb40db0fe40087020e3
Somyansh Rana <somyanshrana80@gmail.com>
2026-09-22 18:04:59 +0530

    Solving Kth Ancestor and Min Dist btw Nodes

--- Binary Tree/LastCommonAncestor.java
+++ Binary Tree/LastCommonAncestor.java
@@ -55,6 +55,24 @@ public class LastCommonAncestor {
         return lca;
     }
 
+    public static Node lca2(Node root, int n1, int n2) {
+        if(root == null || root.data == n1 || root.data == n2) {
+            return root;
+        }
+
+        Node leftLca = lca2(root.left, n1, n2);
+        Node rightLca = lca2(root.right, n1, n2);
+
+        if(rightLca == null) {
+            return leftLca;
+        }
+        if(leftLca == null) {
+            return rightLca;
+        }
+        
+        return root;
+    }
+
     public static void main(String args[]) {
         Node root = new Node(1);
         root.left = new Node(2);
--- Greedy Algorithm/ChocolaProblem.java
+++ Greedy Algorithm/ChocolaProblem.java
@@ -1,7 +1,6 @@
 import java.util.*;
 public class ChocolaProblem {
     public static void main(String args[]) {
-        int n = 4, m = 6;
         Integer costVer[] = {2, 1, 3, 1, 4};
         Integer costHor[] = {4, 1, 2};
 
